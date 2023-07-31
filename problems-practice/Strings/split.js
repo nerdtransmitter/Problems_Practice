2eac3e0ff28a143b56fe0d6fd08a695c21016119
Alessia Moison <adm2217@columbia.edu>
2023-07-31 18:31:13 +0200

    split string into array of strings of 2 chars with underscore after last if uneven length

--- problems-practice/Strings/split.js
+++ problems-practice/Strings/split.js
@@ -1,1 +1,28 @@
-//hey
+// Complete the solution so that it splits the string into pairs of 2 characters.
+// If the string contains an odd number of characters then it should replace
+// the missing second character of the final pair with an underscore ('_').
+
+// Examples:
+// * 'abc' =>  ['ab', 'c_']
+// * 'abcdef' => ['ab', 'cd', 'ef']
+function solution(str){
+  const arr = str.split('');
+  let result = [];
+  let index = 0;
+
+  if (arr.length % 2 === 0) {
+    while (index < arr.length) {
+      result.push(arr.slice(index, index + 2).join(''));
+      index += 2;
+    }
+  } else {
+    while (index < arr.length - 1) {
+      result.push(arr.slice(index, index + 2).join(''));
+      index += 2;
+    }
+    result.push(arr[arr.length - 1] + "_");
+  }
+
+  return result;
+}
+
