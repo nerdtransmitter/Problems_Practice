5875e1689c0964dad59925317ff509b17031349f
Alessia Moison <adm2217@columbia.edu>
2023-07-31 19:01:15 +0200

    sum the squares of numbers in an array

--- problems-practice/Strings/split.js
+++ problems-practice/Strings/split.js
@@ -1,6 +1,5 @@
 // Complete the solution so that it splits the string into pairs of 2 characters.
-// If the string contains an odd number of characters then it should replace
-// the missing second character of the final pair with an underscore ('_').
+// If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
 
 // Examples:
 // * 'abc' =>  ['ab', 'c_']
